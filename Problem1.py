98ed7a9b97f531b43fc4bc020395985368a3b800
0giru <dusrlf13@gmail.com>
2021-01-19 22:53:41 +0900

    큰 수의 법칙

--- Problem1.py
+++ Problem1.py
@@ -14,6 +14,7 @@ list1.sort()
 num_pres = list1[len(list1)-1]
 num_vice = list1[len(list1)-2]
 
+#첫번째 방법
 while True:
     if M == 0:
         break
@@ -23,9 +24,19 @@ while True:
             M -= 1
             if M == 0:
                 break
-            
+
         if(M != 0):
             result += num_vice
             M -= 1
+
+# print(result)
+
+#두번째 방법
+Q = M // (K+1)
+S = M % (K+1)
+
+result = Q * num_pres * K
+result += Q * num_vice
+result += S * num_pres
 
 print(result)
